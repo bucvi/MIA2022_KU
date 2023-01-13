4038137a5b90216d79ac2cbde76c6724ad7437c3
Viktoria Buchegger <viktoria.buchegger@student.tugraz.at>
2023-01-13 15:56:05 +0100

    Add otsu threshold and shape labeling for lung mask

--- 1_Assignment/main.py
+++ 1_Assignment/main.py
@@ -9,30 +9,37 @@ def createNewImageFromArray(voxel_array, template_image):
     result_image.SetDirection(template_image.GetDirection())
     return result_image
 
-
 inputImageFileName = Path("data") / "VESSEL12_05_256.mhd"
-outputImageFileName = Path("VESSEL12_05_256_inverted.mhd")
-outputDenoisedFileName = Path("VESSEL12_05_256_denoised.mhd")
+outputImageFileName = Path("data") / "VESSEL12_05_256_inverted.mhd"
+outputDenoisedFileName = Path("data") / "VESSEL12_05_256_denoised.mhd"
+outputOtsuFileName = Path("data") / "VESSEL12_05_256_otsu.mhd"
+outputShapeLabelFileName = Path("data") / "VESSEL12_05_256_label.mhd"
+outputShapeLabelRegionGrowingFileName = Path("data") / "VESSEL12_05_256_label_region_growing.mhd"
+outputLungMaskFileName = Path("data") / "VESSEL12_05_256_lung_mask.mhd"
+outputLungMaskRegionGrowingFileName = Path("data") / "VESSEL12_05_256_lung_mask_region_growing.mhd"
+
 
 reader = sitk.ImageFileReader()
 reader.SetImageIO("MetaImageIO")
 reader.SetFileName(str(inputImageFileName))
 image = reader.Execute()
 
+writer = sitk.ImageFileWriter()
+
 # this gives us a 'view' on the intensity array, which we can manipulate like a numpy array
 voxel_array = sitk.GetArrayViewFromImage(image)
 min_intensity = voxel_array.min()
 max_intensity = voxel_array.max()
 full_intensity_range = max_intensity - min_intensity
-inverted_voxel_array = full_intensity_range - (voxel_array - min_intensity) + min_intensity
+# inverted_voxel_array = full_intensity_range - (voxel_array - min_intensity) + min_intensity
 
-# we use the inverted voxel values and create a new ITK image again, which requires a copy of the meta information
-# like origin, spacing and orientation!
-inverted_image = createNewImageFromArray(inverted_voxel_array, image)
+# # we use the inverted voxel values and create a new ITK image again, which requires a copy of the meta information
+# # like origin, spacing and orientation!
+# inverted_image = createNewImageFromArray(inverted_voxel_array, image)
 
-writer = sitk.ImageFileWriter()
-writer.SetFileName(str(outputImageFileName))
-writer.Execute(inverted_image)
+# writer = sitk.ImageFileWriter()
+# writer.SetFileName(str(outputImageFileName))
+# writer.Execute(inverted_image)
 
 # Normalize input volume
 normalized_image = (voxel_array - min_intensity)/full_intensity_range
@@ -42,11 +49,11 @@ max_normalized_intensity = normalized_image.max() # Just for testing (should be 
 # Implement ROF primal-dual algorithm
 tau_p = 0.02
 tau_d = 2.0
-lam = 0.1 # Range between 0.1 and 10.0
+lam = 1 # Range between 0.1 and 10.0
 num_iterations = 300
 f = normalized_image
 
-outputDenoisedFileName = Path(f"VESSEL12_05_256_denoised_lambda_{lam}_iterations_{num_iterations}.mhd")
+outputDenoisedFileName = Path("data") / f"VESSEL12_05_256_denoised_lambda_{lam}_iterations_{num_iterations}.mhd"
 
 def gradient(u):
     grad_x = np.diff(u, axis=0, append=0)
@@ -61,7 +68,6 @@ def div(p):
     p_2 = np.diff(p[1], axis=1, prepend=p[1,:,0,:].reshape(dim,1,dim))
     p_3 = np.diff(p[2], axis=2, prepend=p[2,:,:,0].reshape(dim,dim,1))
     return p_1 + p_2 + p_3
-    
 
 def project_p(p):
     return p/np.maximum(1, np.linalg.norm(p, axis=0))
@@ -78,14 +84,50 @@ def rof_denoising(f, lam, num_iterations, tau_p, tau_d):
     print("Iteration:", num_iterations)
     print("Finished denoising!")
     return u, p
+load_denoised = True
 
-u, p = rof_denoising(f, lam, num_iterations, tau_p, tau_d)
+if load_denoised:
+    reader.SetFileName(str(outputDenoisedFileName))
+    denoised_image = reader.Execute()
+else:
+    u, p = rof_denoising(f, lam, num_iterations, tau_p, tau_d)
 
-denoised_image = createNewImageFromArray(u, image)
-writer = sitk.ImageFileWriter()
-writer.SetFileName(str(outputDenoisedFileName))
-writer.Execute(denoised_image)
+    denoised_image = createNewImageFromArray(u, image)
+    writer.SetFileName(str(outputDenoisedFileName))
+    writer.Execute(denoised_image)
 
+# Apply an Otsu Threshold
+num_bins = 128
 
+otsu_filter = sitk.OtsuThresholdImageFilter()
+segmented_image = otsu_filter.Execute(denoised_image)
 
+writer.SetFileName(str(outputOtsuFileName))
+writer.Execute(segmented_image)
 
+# Shape Labeling
+connected_component_filter = sitk.ConnectedComponentImageFilter()
+shape_label_image = connected_component_filter.Execute(image, segmented_image)
+
+labeled_image = sitk.GetArrayFromImage(shape_label_image)
+labels = np.unique(labeled_image)
+
+writer.SetFileName(str(outputShapeLabelFileName))
+writer.Execute(shape_label_image)
+
+# Choose lung
+segments = sitk.GetArrayFromImage(segmented_image)
+components = np.zeros(labels.shape + (3,))
+for label in labels:
+    mask = labeled_image==label
+    components[label, 0] = label
+    components[label, 1] = segments[mask][...,0]
+    components[label, 2] = mask.sum()
+
+dark_components = np.delete(components[components[:,1]==1], 1, 1)
+lung_label = dark_components[dark_components[:,1].argsort()][-2,0]
+lung_mask = (labeled_image==lung_label).astype(np.uint8)
+
+lung_mask_image = createNewImageFromArray(lung_mask, image)
+writer.SetFileName(str(outputLungMaskFileName))
+writer.Execute(lung_mask_image)
